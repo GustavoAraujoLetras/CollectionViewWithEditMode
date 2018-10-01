432312f2ef8c53902f7848c8857dfb04eaa0a298
Gustavo Araújo <gustavo.araujo@letras.mus.br>
2018-10-01 14:51:06 -0300

    Initial Commit

---
+++ CollectionViewWithEditMode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionViewWithEditMode
+//
+//  Created by Gustavo Araújo on 01/10/18.
+//  Copyright © 2018 Letras. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
